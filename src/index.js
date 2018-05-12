58d41c969eadcd1f8dc798d772c37e8eec41e22d
piotrek <pszklanko@gmail.com>
2018-05-12 14:36:28 +0200

    arrays part 2

--- src/index.js
+++ src/index.js
@@ -1,42 +1,42 @@
 import './style.css';
 
-const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+const people = [
+    { name: 'Wes', year: 1988 },
+    { name: 'Kait', year: 1986 },
+    { name: 'Irv', year: 1970 },
+    { name: 'Lux', year: 2015 }
+];
 
-const cities = [];
+const comments = [
+    { text: 'Love this!', id: 523423 },
+    { text: 'Super good', id: 823423 },
+    { text: 'You are the best', id: 2039842 },
+    { text: 'Ramen is my fav food ever', id: 123523 },
+    { text: 'Nice Nice Nice!', id: 542328 }
+];
 
-fetch(endpoint)
-    .then(blob => blob.json())
-    .then(data => cities.push(...data))
+const currentYear = new Date().getFullYear();
 
-function findMatches(wordToMatch, cities) {
-    return cities.filter(place => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.city.match(regex) || place.state.match(regex);
-    })
-}
+// Some and Every Checks
+// Array.prototype.some() // is at least one person 19 or older?
+const hasAdults = people.some(person => currentYear - person.year >= 19);
 
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-}
+// Array.prototype.every() // is everyone 19 or older?
+const onlyAdults = people.every(person => currentYear - person.year >= 19);
 
-function displayMatches() {
-    const matchArray = findMatches(this.value, cities);
-    const html = matchArray.map(place => {
-        const regex = new RegExp(this.value, 'gi');
-        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-        return `
-          <li>
-            <span class="name">${cityName}, ${stateName}</span>
-            <span class="population">${numberWithCommas(place.population)}</span>
-          </li>
-        `
-    }).join('')
-    suggestions.innerHTML = html;
-}
+// Array.prototype.find()
+// Find is like filter, but instead returns just the one you are looking for
+// find the comment with the ID of 823423
+const hasId = comments.find(elem => elem.id === 823423);
 
-const searchInput = document.querySelector('.search');
-const suggestions = document.querySelector('.suggestions');
+// Array.prototype.findIndex()
+// Find the comment with this ID
+// delete the comment with the ID of 823423
+const commentIndex = comments.findIndex(elem => elem.id === 823423);
+comments.splice(commentIndex, 1);
 
-searchInput.addEventListener('change', displayMatches);
-searchInput.addEventListener('keyup', displayMatches);
+// not modifying comments array
+// const secondOption = [
+//     ...comments.slice(0, commentIndex),
+//     ...comments.slice(commentIndex + 1)
+// ];
